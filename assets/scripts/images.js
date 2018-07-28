2ab26dbfb5a2b18a060789fe6b557656a0af6c6e
pavokujaku <jjpavo@my.uri.edu>
2018-07-27 23:19:52 -0400

    Started handling labels
    Show input on image drop. Hide input and show label when a value is entered. Hide label and show input when the label is clicked.

--- assets/scripts/images.js
+++ assets/scripts/images.js
@@ -1,4 +1,3 @@
-//https://www.w3schools.com/html/html5_draganddrop.asp
 function allowDrop(ev) {
   ev.preventDefault();
 }
@@ -10,12 +9,63 @@ function drag(ev) {
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data).cloneNode(true));
+  var img = document.getElementById(data);
+  ev.target.appendChild(img.cloneNode(true));
   handleLabel(ev.target);
 }
 
-function handleLabel(entry) {
-  console.log(entry.id);
-}
+// Needed since handleLabel needs to be defined after the document is ready, but
+// also needs to be defined before, for the drag&drop
+function handleLabel(entry) {}
 
-$("body").removeClass("preload");
+$(document).ready(function() {
+  handleLabel = function(entry) {
+    var input = document.getElementById(entry.id + "-input");
+    input.style.display = "block";
+    // If the element before the label-input pair is a the number for a new row then show it.
+    // previousELementSibling twice because it goes span -> label -> input.
+    if(input.previousElementSibling.previousElementSibling.tagName === "SPAN") {
+      input.previousElementSibling.previousElementSibling.style.display = "block";
+    }
+  }
+
+  var entryInputs = document.getElementsByClassName('entry-input');
+  var entryLabels = document.getElementsByClassName('entry-label');
+
+  for(var i in entryInputs) {
+    var input = entryInputs[i];
+    if(input.id !== undefined) {
+      input.addEventListener("keydown", function(event) {
+        if (event.repeat) { return } //debounce
+        if (event.keyCode === 13) {
+          // Show label with user entry and hide text input box.
+          var label = document.getElementById(event.target.id.split("-")[0] + "-label");
+          label.style.display = "block";
+
+          if(event.target.value === "") {
+            label.innerText = "\"empty\"";
+          } else {
+            label.innerText = event.target.value;
+          }
+
+          event.target.style.display = "none";
+        }
+      });
+    }
+  }
+
+  for(var i in entryLabels) {
+    var label = entryLabels[i];
+    if(label.id !== undefined) {
+      label.addEventListener("click", function(event) {
+        // Show label with user entry and hide text input box.
+        var input = document.getElementById(event.target.id.split("-")[0] + "-input");
+        input.style.display = "block";
+        input.value = event.target.innerText;
+        event.target.style.display = "none";
+      });
+    }
+  }
+
+  $("body").removeClass("preload");
+});
